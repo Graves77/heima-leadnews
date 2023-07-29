4b577dda914ef4244de451ac7cd544d3a49934f3
Graves77 <870519251@qq.com>
2023-07-29 23:46:04 +0800

    7.29 自媒体文章发布

---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/WmNewsService.java
@@ -0,0 +1,24 @@
+package com.heima.wemedia.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.wemedia.dtos.WmNewsDto;
+import com.heima.model.wemedia.dtos.WmNewsPageReqDto;
+import com.heima.model.wemedia.pojos.WmNews;
+
+import java.lang.reflect.InvocationTargetException;
+
+public interface WmNewsService extends IService<WmNews> {
+
+    /**
+     * 条件查询文章列表
+     */
+    public ResponseResult findList(WmNewsPageReqDto dto);
+
+    /**
+     * 保存发布文章
+     * @param dto
+     * @return
+     */
+    public ResponseResult submitNews(WmNewsDto dto);
+}
