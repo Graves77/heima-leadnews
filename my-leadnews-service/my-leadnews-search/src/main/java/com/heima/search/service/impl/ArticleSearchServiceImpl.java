84a9e08e8da7b89e519a437fd1296717a13e8e6e
Graves77 <870519251@qq.com>
2023-08-31 21:34:07 +0800

    8.31

--- my-leadnews-service/my-leadnews-search/src/main/java/com/heima/search/service/impl/ArticleSearchServiceImpl.java
+++ my-leadnews-service/my-leadnews-search/src/main/java/com/heima/search/service/impl/ArticleSearchServiceImpl.java
@@ -1,10 +1,13 @@
 package com.heima.search.service.impl;
-import com.alibaba.fastjson.JSON;
 
+import com.alibaba.fastjson.JSON;
 import com.heima.model.common.dtos.ResponseResult;
 import com.heima.model.common.enums.AppHttpCodeEnum;
 import com.heima.model.search.dtos.UserSearchDto;
+import com.heima.model.user.pojos.ApUser;
+import com.heima.search.service.ApUserSearchService;
 import com.heima.search.service.ArticleSearchService;
+import com.heima.utils.thread.AppThreadLocalUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.elasticsearch.action.search.SearchRequest;
@@ -32,6 +35,9 @@ public class ArticleSearchServiceImpl implements ArticleSearchService {
     @Autowired
     private RestHighLevelClient restHighLevelClient;
 
+    @Autowired
+    private ApUserSearchService apUserSearchService;
+
     /**
      * es文章分页检索
      *
@@ -45,6 +51,14 @@ public class ArticleSearchServiceImpl implements ArticleSearchService {
         if(dto == null || StringUtils.isBlank(dto.getSearchWords())){
             return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
         }
+
+        ApUser user = AppThreadLocalUtil.getUser();
+
+        //异步调用 保存搜索记录
+        if(user != null && dto.getFromIndex() == 0){
+            apUserSearchService.insert(dto.getSearchWords(), user.getId());
+        }
+
 
         //2.设置查询条件
         SearchRequest searchRequest = new SearchRequest("app_info_article");
